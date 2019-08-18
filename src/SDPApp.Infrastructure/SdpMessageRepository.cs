4ac8cbd790b80bdf367d2ba5bce302a9f7ad664d
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-08-18 16:15:42 +0300

    adds message repository implementation - infrastructure

---
+++ src/SDPApp.Infrastructure/SdpMessageRepository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using SDPApp.Core.Abstraction;
+
+namespace SDPApp.Infrastructure
+{
+    public class SdpMessageRepository:ISdpMessageRepository
+    {
+        private string _filePath;
+        public SdpMessageRepository(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        
+        public async Task<IEnumerable<string>> GetMessages()
+        {
+            var sdpFileContent = File.ReadAllText(_filePath);
+            var seperator = $"\r\n\r\n";
+            var messages = sdpFileContent.Split(new[] {seperator}, StringSplitOptions.RemoveEmptyEntries);
+            return messages;
+        }
+    }
+}
