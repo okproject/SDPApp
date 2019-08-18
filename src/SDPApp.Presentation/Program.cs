4a9e34d83b2b862264eb1eebec8dc905dce879e9
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-08-18 18:29:51 +0300

    adds explanation for where output file is

--- src/SDPApp.Presentation/Program.cs
+++ src/SDPApp.Presentation/Program.cs
@@ -23,10 +23,11 @@ namespace SDPApp.Presentation
             var extractorSettings = new ExtractorSettings()
             {
                 MaxDegreeOfParallelism = Environment.ProcessorCount,
-                OutputFileFullPath = $"{Environment.CurrentDirectory}/result-{Guid.NewGuid().ToString()}.txt"
+                OutputFileFullPath =
+                    $"{Environment.CurrentDirectory}/result-{Guid.NewGuid().ToString()}.txt" //File will be saved here
             };
             var sdpMessagesFilePath =
-                @"/Users/rasihcaglayan/Documents/temp/base/sdp_input_huge.txt"; //source sdp message file. this file will be using extracting data.
+                @"/Users/rasihcaglayan/Documents/temp/base/sdp_input.txt"; //source sdp message file. this file will be using extracting data.
 
             var ipParser = new IpParser();
             var portParser = new PortParser();
@@ -41,7 +42,10 @@ namespace SDPApp.Presentation
             //YOU CAN OBSERVER RESULT IN THIS VARIABLE WITH BREAKPOINT: resultMessages
             var resultMessages =
                 extractedData
-                    .ExtractedMessages; //results will be saved path specified in extractorSettings.OutputFileFullPath variable.
+                    .ExtractedMessages; //results will be saved path specified in extractorSettings.OutputFileFullPath variable; you can find it from starting lines of this method.
+
+            Console.WriteLine($"File saved to: {extractorSettings.OutputFileFullPath}"); //Find where file saved
+            Console.WriteLine($"Output format : IP;PORT;codec1-codec2-codec-n"); // This is the format that i am using to save file. It has same order with source data.
         }
     }
 }
