0c2759d580406c17c67f967ce52e3456c6d66f06
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-08-18 15:49:21 +0300

    adds prototype call for presentation

--- src/SDPApp.Presentation/Program.cs
+++ src/SDPApp.Presentation/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using SDPApp.Application.UseCase;
+using SDPApp.Core.Abstraction;
 
 namespace SDPApp.Presentation
 {
@@ -7,6 +11,18 @@ namespace SDPApp.Presentation
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static async Task ExtractData()
+        {
+//            ISdpMessageRepository repository;
+//            ISdpExtractor _sdpExtractor;
+//            
+//            var query=new GetExtractedMessagesQuery();
+//            var handler=new GetExtractedMessagesQueryHandler(repository,_sdpExtractor);
+//            var extractedData =await  handler.Handle(query, new CancellationToken());
+//            extractedData.
+
         }
     }
 }
