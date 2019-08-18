a3eeb1607cae9da67cabb30ddc78e36d89330672
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-08-18 17:19:58 +0300

    adds output model - extractedMessage

--- src/SDPApp.Core/ExtractedMessage.cs
+++ src/SDPApp.Core/ExtractedMessage.cs
@@ -1,8 +1,19 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
 namespace SDPApp.Core
 {
     public class ExtractedMessage
     {
-    
-        
+        public string Ip { get; set; }
+        public string Port { get; set; }
+        public IEnumerable<string> Codecs { get; set; }
+
+        public override string ToString()
+        {
+            var result = $"{Ip};{Port};{string.Join("-", Codecs)}";
+            result = Regex.Replace(result, @"\t|\n|\r", "");
+            return result;
+        }
     }
 }
